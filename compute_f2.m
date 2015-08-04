aa73f14b43de03b69e86b6f0baddd12fce333700
minjay <mjfan@ucar.edu>
2015-08-04 12:55:48 -0600

    simplify f2

--- compute_f2.m
+++ compute_f2.m
@@ -1,14 +1,7 @@
 function f2_value = compute_f2( u )
 
-neg_u = -abs(u);
-
 f1 = @(x) exp(-1./(1-x.^2));
 
-f2_value = integral(f1, -1, neg_u)/integral(f1, -1, 1);
-
-%\psi(u)=1-\psi(-u) if u>0
-if u>0
-    f2_value = 1-f2_value;
-end
+f2_value = integral(f1, -1, u)/integral(f1, -1, 1);
 
 end
