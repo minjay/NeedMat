f0f6da8cdcb09b281a6f16e9744805694b797d23
minjay <mjfan@ucar.edu>
2015-08-06 15:51:45 -0600

    fast needlet evaluation

---
+++ spneedlet_eval_fast.m
@@ -0,0 +1,18 @@
+function psi = spneedlet_eval_fast(B, j, bl_vector, dist, sqrt_lambda)
+
+l_min = ceil(B^(j-1));
+l_max = floor(B^(j+1));
+
+n = length(dist);
+
+P = p_polynomial_value( n, l_max, dist );
+
+psi = zeros(n, 1);
+
+for i = 1:n
+    psi(i) = sum(bl_vector(j+1, l_min:l_max).*P(i, l_min+1:l_max+1));
+end
+
+psi = psi*sqrt_lambda;
+
+end
