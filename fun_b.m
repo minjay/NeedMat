12d25be034e92d8e1dce5fcbc4dfe0489baa1642
minjay <mjfan@ucar.edu>
2015-08-06 15:51:26 -0600

    deal with numerical error

--- fun_b.m
+++ fun_b.m
@@ -1,5 +1,10 @@
 function b_value = fun_b( x, B )
 
-b_value = sqrt( compute_f3(x/B, B)-compute_f3(x, B) );
+
+b_value =  compute_f3(x/B, B)-compute_f3(x, B);
+if b_value<0
+    b_value = 0;
+end
+b_value = sqrt(b_value);
 
 end
