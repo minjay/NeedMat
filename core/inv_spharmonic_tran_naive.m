2d32055acfed2066f9f25922c7609c45fe9ac235
minjay <mjfan@ucar.edu>
2015-08-12 12:11:08 -0600

    rename

--- core/inv_spharmonic_tran_naive.m
+++ core/inv_spharmonic_tran_naive.m
@@ -1,8 +1,11 @@
-function map = inv_spharmonic_tran_irr(alm, theta, phi, l_max)
+function map = inv_spharmonic_tran_naive(alm, theta, phi, l_max)
 
 n = length(theta);
 map = zeros(n, 1);
 for i = 1:n
+    if mod(i, 1000)==0
+        i
+    end
     for l = 0:l_max
         for m = 0:l
             if m==0
