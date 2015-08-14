616f898e0a96efe27d55ef34b475cfc9d913b9df
minjay <mjfan@ucar.edu>
2015-08-14 16:23:07 -0600

    add comments

--- core/spharmonic_tran_irr.m
+++ core/spharmonic_tran_irr.m
@@ -1,4 +1,23 @@
 function alm = spharmonic_tran_irr(theta, phi, f, l_max)
+%SPHARMONIC_TRAN_IRR   Spherical harmonic transform for irregularly spaced 
+%observations of function f at locations (theta, phi). It estimates the 
+%spherical harmonic coefficients empirically using the weights determined 
+%by the Voronoi diagram of these locations on the sphere.
+%
+%   alm = spharmonic_tran_irr(theta, phi, f, l_max)
+%
+% Inputs:
+%   theta - the co-latitude of the locations, N-by-1 vector
+%   phi - the longitude of the locations, N-by-1 vector
+%   f - the values of the function at the locations
+%   l_max - the maximal l
+%
+% Outputs:
+%   alm - the spherical harmonic coefficients, (l_max+1)-by-(2*l_max+1)
+%   matrix, alm(l+1, m+l_max+1) is the spherical harmonic coefficient with
+%   subscripts l and m
+%
+% Author: Minjie Fan, 2015
 
 n = length(theta);
 
