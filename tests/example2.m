7d3bd5a124c8db1cff9b61cffeb1c7882a8be6ae
minjay <mjfan@ucar.edu>
2015-08-13 20:45:09 -0600

    example 2

---
+++ tests/example2.m
@@ -0,0 +1,30 @@
+B = 2;
+
+rep = 10;
+time = zeros(8, rep);
+
+for power = 2:9
+    l_max = 2^power;
+    alm = zeros(l_max+1, 2*l_max+1);
+    for r = 1:rep
+        for l = 1:l_max
+            for m = 0:l
+                alm(l+1, m+l_max+1) = randn+1i*randn;
+            end
+        end
+        tic
+        beta = spneedlet_tran( alm, l_max, B );
+        time(power-1, r) = toc;
+    end
+end
+
+save('run_time.mat', 'time')
+
+boxplot(time', 'labels', {'4', '8', '16', '32', '64', '128', '256', '512'})
+med = median(time, 2);
+hold on
+plot(1:8, med, '--o', 'LineWidth', 1.5)
+xlabel('l_{max}', 'FontSize', 15)
+ylabel('Time (s)', 'FontSize', 15)
+axis tight
+set(gca, 'YScale', 'log') 
