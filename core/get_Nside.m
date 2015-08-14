6c278dbad94fea7e5850e84ea95cbe995ff48ba3
minjay <mjfan@ucar.edu>
2015-08-14 15:23:26 -0600

    add comments

--- core/get_Nside.m
+++ core/get_Nside.m
@@ -1,4 +1,15 @@
 function Nside = get_Nside(B, j)
+%GET_NSIDE   Computes Nside.
+%
+%   Nside = get_Nside(B, j)
+% Inputs:
+%   B - the parameter
+%   j - the frequency
+%
+% Outputs:
+%   Nside - the Nside
+%
+% Author: Minjie Fan, 2015
 
 Nside = 1;
 lb = floor(B^(j+1));
--- core/get_j_max.m
+++ core/get_j_max.m
@@ -1,4 +1,16 @@
 function j_max = get_j_max(B, l_max)
+%GET_J_MAX   Computes the maximal j.
+%
+%   j_max = get_j_max(B, l_max)
+%
+% Inputs:
+%   B - the parameter
+%   l_max - the maximal l
+%
+% Outputs:
+%   j_max - the maximal j
+%
+% Author: Minjie Fan, 2015
 
 j_max = 0;
 while ceil(B^(j_max-1))<=l_max
